e4198b17d902ae02e6dac24742c5d9b8aad70189
anissiam <anis.siam@hotmail.com>
2023-09-23 11:39:36 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //this line to print your name
+        System.out.println("maryana");  //طابعة نتائج داخل ال console
+
+        //System.out.println("maryana");
+
+        /*System.out.println("maryana");
+        System.out.println("maryana");*/
+
+    }
+
+}
+
+//Java Identifiers // التعريف
+/*
+Class
+
+
+             UserList
+   T L          T
+   1 ممنوع
+   $ _         $ _
+   ممنوع +/@!
+
+
+//method()  , var
+// object , method , var
+        userList
+ */
+
+//this code to get all user from db
+
+
+//problem solver
+
+//front end development
+//html //css //js  //pure front end dev
+
+//react js //next js  //library
+
+
+//back end
+//node js
---
+++ src/User.java
@@ -0,0 +1,188 @@
+import java.util.Calendar;
+
+public class User {
+    public static void main(String[] args) {
+
+/*
+        int i = 0;
+
+        System.out.println(i + 1);
+        System.out.println(++i);
+        System.out.println(1+1);
+        System.out.println(i++);
+        i += 3;
+        System.out.println(i++);
+        System.out.println(i--);
+        System.out.println(--i);
+        System.out.println(i);*/
+
+
+      /*  int i = 0;
+                                                  //Ram-> i=0,1,2,3 ,2 ,1 ,2
+                                                  //Console->1 , 1 ,3 ,2,2,3,2
+        System.out.println(i + 1);
+        i++; //i=i+1
+        System.out.println(i++); // 1)print i    2) i=i+1
+        System.out.println(++i);// i=i+1       print i
+        System.out.println(--i);//i=i-1     print i
+        --i; //i=i-1
+        System.out.println(++i);//i=i+1   print i
+        System.out.println(i + 1);
+        System.out.println(i);*/
+
+
+        /*int i = 1;
+        System.out.println(i++); //i= i +1
+
+        System.out.println(i);*/
+
+        // 1 => 1 +1 ==> 2
+
+        /*int x = 1;
+        x = x * 90; // x*=90*/
+
+
+/*
+int age =28;
+
+// 28 => age + 5 => age
+//age=+age5;
+        age+=5;
+*/
+
+
+
+
+
+       /* int x = 5;
+        int y = 10;
+        int z = 30;
+
+        int result = (x + y + z) / 3;
+
+        System.out.println(result);*/
+
+        //System.out.println("my ava = " + (30 + 3));
+
+     /*   int x = 10;
+        System.out.println(x % 3);*/
+
+      /*  int x = 10;
+        //x = x % 3;
+        x %= 3;
+
+        System.out.println(x);
+        */
+
+
+
+       /* int i = 1;
+       // i += 1; //i = i + 1;
+        i++; //++i
+
+        System.out.println(i);*/
+
+
+
+
+       /* long time = Calendar.getInstance().getTimeInMillis();
+        System.out.println(time);*/
+
+        /*
+        myInt = 10
+        myByte = myInt
+        اطبع myByte
+         */
+
+
+//        long myNum = (long) 500.5;
+
+        /*int x =(int)3.9999999;
+        //32  =64
+        //32 = 32 (int)
+        System.out.println(x);*/
+
+
+
+
+        //int num; //null
+        /*int num = 500;
+
+        System.out.println("The num is " + num);
+
+        num = 400;
+        System.out.println("Num ="+num);*/
+
+
+        /*int age = 28;
+
+        int newAge = age;
+        System.out.println("Meryana age " + newAge + " and her hir is brown 50" + " sdsadskfcmlkzdmvkl" + 50.1);
+*/
+        /*System.out.println();
+
+        double d = 50.3;
+        System.out.println(d);
+        d = 80.7;
+
+        //float dd = 8.3;
+        //32bit  = 64 bit
+
+        //float f =(float) 10.9;
+        float dd =10.8f;
+        System.out.println(dd);
+
+
+        double ee = 30;
+        //64     = 32
+        System.out.println(ee);
+*/
+
+
+
+
+
+
+
+
+
+        /*int x = 5;
+        x = 10;
+        System.out.println(x);
+        byte mark = 50;
+        System.out.println(mark);
+        int price = 0;
+        System.out.println(price);*/
+
+
+    }
+}
+//تعريف المتغيرات    //variables declaration
+//س = 5
+
+
+// data type  varName = value
+// size :
+
+
+//int x=5 ;
+//size: 32bit
+
+
+//data type
+//primitive
+//Nemaric
+//Inegers صحيح
+//byte  , short , int , long
+// 8    ,  16   , 32   , 64   bit
+//Floating point عشري
+//float  , double
+//32     ,  64    bit
+
+//non - nemaric
+//char
+//boolean (true , false)
+
+
+//non-primitive
+
